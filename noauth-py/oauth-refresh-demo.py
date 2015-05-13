a4ef701366efa99a6f507cccb31b244048dbc7c0
Ubuntu <nich-1@matasano.com>
2015-05-13 21:06:36 +0000

    Fixed the local session and can retrieve OAuth token
    No longer frustrated by a failing session. OAuth token is now
    submitted successfully. Woot!

--- noauth-py/oauth-refresh-demo.py
+++ noauth-py/oauth-refresh-demo.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 
 # This information is obtained upon registration of a new Google OAuth
 # application at https://code.google.com/apis/console
-client_id = getenv('GOOGLE_CLIENT_ID') 
-client_secret = getenv('GOOGLE_CLIENT_SECRET') 
+google_client_id = getenv('GOOGLE_CLIENT_ID') 
+google_client_secret = getenv('GOOGLE_CLIENT_SECRET') 
 redirect_uri = 'http://ec2-52-8-68-137.us-west-1.compute.amazonaws.com:5000/oauth2callback'
 
 # Uncomment for detailed oauthlib logs
@@ -23,23 +23,23 @@ redirect_uri = 'http://ec2-52-8-68-137.us-west-1.compute.amazonaws.com:5000/oaut
 #log.setLevel(logging.DEBUG)
 
 # OAuth endpoints given in the Google API documentation
-authorization_base_url = "https://accounts.google.com/o/oauth2/auth"
-token_url = "https://accounts.google.com/o/oauth2/token"
-refresh_url = token_url # True for Google but not all providers.
+google_auth_base_url = "https://accounts.google.com/o/oauth2/auth"
+google_token_url = "https://accounts.google.com/o/oauth2/token"
+refresh_url = google_token_url # True for Google but not all providers.
 scope = [
     "https://www.googleapis.com/auth/userinfo.email",
     "https://www.googleapis.com/auth/userinfo.profile",
 ]
 
-@app.route("/")
-def demo():
+@app.route("/oauth2/google")
+def google_oauth():
     """Step 1: User Authorization.
 
     Redirect the user/resource owner to the OAuth provider (i.e. Google)
     using an URL with a few key OAuth parameters.
     """
-    google = OAuth2Session(client_id, scope=scope, redirect_uri=redirect_uri)
-    authorization_url, state = google.authorization_url(authorization_base_url,
+    google = OAuth2Session(google_client_id, scope=scope, redirect_uri=redirect_uri)
+    authorization_url, state = google.authorization_url(google_auth_base_url,
         # offline for refresh token
         # force to always make user click authorize
         access_type="offline", approval_prompt="force")
@@ -50,7 +50,7 @@ def demo():
 
 
 # Step 2: User authorization, this happens on the provider.
-@app.route("/callback", methods=["GET"])
+@app.route("/oauth2callback", methods=["GET"])
 def callback():
     """ Step 3: Retrieving an access token.
 
@@ -59,9 +59,9 @@ def callback():
     in the redirect URL. We will use that to obtain an access token.
     """
 
-    google = OAuth2Session(client_id, redirect_uri=redirect_uri,
-                           state=session['oauth_state'])
-    token = google.fetch_token(token_url, client_secret=client_secret,
+    google = OAuth2Session(google_client_id, redirect_uri=redirect_uri,
+                           state=session.pop('oauth_state', None))
+    token = google.fetch_token(google_token_url, google_client_secret=google_client_secret,
                                authorization_response=request.url)
 
     # We use the session as a simple DB for this example.
@@ -82,9 +82,8 @@ def menu():
         <li><a href="/manual_refresh"> Explicitly refresh the token</a></li>
         <li><a href="/validate"> Validate the token</a></li>
     </ul>
-
     <pre>
-    %s
+        %s
     </pre>
     """ % pformat(session['oauth_token'], indent=4)
 
@@ -93,7 +92,7 @@ def menu():
 def profile():
     """Fetching a protected resource using an OAuth 2 token.
     """
-    google = OAuth2Session(client_id, token=session['oauth_token'])
+    google = OAuth2Session(google_client_id, token=session.pop('oauth_token', None))
     user_info_url = 'https://www.googleapis.com/oauth2/v1/userinfo'
     return jsonify(google.get(user_info_url).json())
 
@@ -110,14 +109,14 @@ def automatic_refresh():
     token['expires_at'] = time() - 10
 
     extra = {
-        'client_id': client_id,
-        'client_secret': client_secret,
+        'client_id': google_client_id,
+        'client_secret': google_client_secret,
     }
 
     def token_updater(token):
         session['oauth_token'] = token
 
-    google = OAuth2Session(client_id,
+    google = OAuth2Session(google_client_id,
                            token=token,
                            auto_refresh_kwargs=extra,
                            auto_refresh_url=refresh_url,
@@ -135,11 +134,11 @@ def manual_refresh():
     token = session['oauth_token']
 
     extra = {
-        'client_id': client_id,
-        'client_secret': client_secret,
+        'client_id': google_client_id,
+        'client_secret': google_client_secret,
     }
 
-    google = OAuth2Session(client_id, token=token)
+    google = OAuth2Session(google_client_id, token=token)
     session['oauth_token'] = google.refresh_token(refresh_url, **extra)
     return jsonify(session['oauth_token'])
 
